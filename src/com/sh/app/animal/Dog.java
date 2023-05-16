f4c65ca79036d545bca848f78c819f9be07122b9
so_young <my5126@naver.com>
2023-05-16 16:40:37 +0900

    dog에 master메소드 추가

--- src/com/sh/app/animal/Dog.java
+++ src/com/sh/app/animal/Dog.java
@@ -5,5 +5,10 @@ public class Dog {
 	public static void main(String[] args) {
 			System.out.println("강아지는 귀엽지, 호두도 귀엽지");
 	}
+	
+	private void master() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
