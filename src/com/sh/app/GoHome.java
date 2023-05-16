e901d66fd76235caf7f56bd70f89992679272f3a
so_young <my5126@naver.com>
2023-05-16 15:43:54 +0900

    CreateGoHome.java

---
+++ src/com/sh/app/GoHome.java
@@ -0,0 +1,12 @@
+package com.sh.app;
+
+public class GoHome {
+
+	public static void main(String[] args) {
+
+		System.out.println("I want to go home");
+		System.out.println("Let me go home");
+		
+	}
+
+}
--- src/com/sh/app/Run.java
+++ src/com/sh/app/Run.java
@@ -6,6 +6,8 @@ public class Run {
 
 		System.out.println("Hello World");
 		
+		
+		
 	}
 
 }
