15e55331ec4e648b02d26c12568f63ef83692f34
so_young <my5126@naver.com>
2023-05-16 16:44:12 +0900

    Dog#master메소드

--- src/com/sh/app/animal/Dog.java
+++ src/com/sh/app/animal/Dog.java
@@ -1,17 +1,7 @@
 package com.sh.app.animal;
 
 public class Dog {
-
-	public static void main(String[] args) {
-			System.out.println("강아지는 귀엽지, 호두도 귀엽지");
-	}
-	
 	private void master() {
-		// TODO Auto-generated method stub
-
-	}
-
-	private void animal() {
 
 	}
 }
